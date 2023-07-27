edac52b4afd15828b0c128d268fed8c60e47153e
SUDESHNA RAKSHIT <123752642+SUDESHNARAKSHIT@users.noreply.github.com>
2023-07-27 15:01:16 +0530

    Add files via upload

---
+++ src/AppLoader.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import AppContext from "./AppContext";
+import logo from "./ui/logo-coral.svg";
+
+import "./css/uifont.css";
+import "./css/props.css";
+import "./css/App.css";
+
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+
+//Screen
+import Header from "./screens/header";
+import Sidebar from "./screens/sidebar";
+import HomePage from "./screens/home";
+import Rightbar from "./screens/rightbar";
+import CoursePage from "./screens/course";
+import DiscoverPage from "./screens/discover";
+import CategoriesPage from "./screens/categories";
+import MyCoursesPage from "./screens/mycourses";
+
+import AccountPage from "./screens/oauth";
+
+import * as fire_base from "firebase";
+global.firebase = fire_base;
+global.fire = {
+    ID: null
+};
+const firebaseConfig = {
+    apiKey: "AIzaSyCpmt6abXSzDADPp4NxP0Ih08SmrTB3YRk",
+    authDomain: "udemyclone-2a5f8.firebaseapp.com",
+    projectId: "udemyclone-2a5f8",
+    storageBucket: "udemyclone-2a5f8.appspot.com",
+    messagingSenderId: "352342131747",
+    appId: "1:352342131747:web:ffb7610080d09ec4cf5d96",
+    measurementId: "G-RD2JW5TM1D"
+  };
+// Initialize Firebase
+global.firebase.initializeApp(firebaseConfig);
+
+
+
+export default function AppLoader(){
+
+    const [isFireUser, setIsFireUser] = useState(false);
+
+    const initFirebase = async (context) => {
+        global.firebase.auth().onAuthStateChanged((user)=>{
+          if(user){
+              console.log("You are signed in...")
+              setIsFireUser(true);
+          }else{
+            console.log("You are guest...");
+            setIsFireUser(false);
+            setTimeout(()=>{
+                context.setAppLoaded(true);
+            }, 500);
+          }
+        });
+    }
+
+    const splash = (context) => {
+        return (
+            <div className="App flex">      
+                <div className="splash abs abc">
+                    <img src={logo} className="bl" />
+                </div>
+            </div>
+        )
+    }
+
+    const loadApp = async (context) => {
+        await initFirebase(context);
+    }
+
+    return (
+        <AppContext.Consumer>
+            {
+                context => {
+                    return (
+                        context.appLoaded() ? 
+                        <div className="App flex">      
+                            <HashRouter>
+                                <Sidebar />
+                                <div className="app-content">
+                                    <Route exact path="/" component={HomePage} />
+                                    <Route path="/course/:courseid" component={CoursePage} />
+                                    <Route path="/discover" component={DiscoverPage} />
+                                    <Route path="/cates" component={CategoriesPage} />
+                                    <Route path="/my-courses" component={MyCoursesPage} />
+                                    <Route path="/oauth" component={AccountPage} />
+                                </div>    
+                            </HashRouter>    
+                        </div>
+                        : 
+                        <AppContext.Consumer>
+                            {
+                                context => {
+                                    loadApp(context);
+                                    return (splash(context))
+                                }
+                            }
+                        </AppContext.Consumer>
+                    )
+                }
+            }
+        </AppContext.Consumer>
+    )
+
+}
